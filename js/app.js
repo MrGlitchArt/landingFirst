af9340ecaac1439a080e7966e8cb3ee18578c5c2
Mr_Glitch <art.mrglitch@gmail.com>
2023-08-25 02:04:51 +0200

    removed touch

--- js/app.js
+++ js/app.js
@@ -12,14 +12,3 @@ window.addEventListener("mousemove", (e) => {
         el.style.transform = `translateX(calc(-50% + ${xVal * speedx}px)) translateY(calc(-50% + ${yVal * speedy}px))`;
     });
 });
-
-window.addEventListener("touchstart", (e) => {
-    xVal = e.clientX - window.innerWidth / 2;
-    yVal = e.clientY - window.innerHeight / 2;
-
-    parallax_el.forEach((el) => {
-        let speedx = el.dataset.speedx;
-        let speedy = el.dataset.speedy;
-        el.style.transform = `translateX(calc(-50% + ${xVal * speedx}px)) translateY(calc(-50% + ${yVal * speedy}px))`;
-    });
-});
