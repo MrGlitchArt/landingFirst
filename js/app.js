d19445390093710ef695e7c8d399f6d477b181c5
Mr_Glitch <art.mrglitch@gmail.com>
2023-10-10 00:03:15 +0200

    added deviceorientation for mobile

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
 const parallax_el = document.querySelectorAll(".parallax");
 
-let xVal = 0, yVal = 0;
+    let xVal = 0, yVal = 0;
 
-window.addEventListener("mousemove", (e) => {
-    xVal = e.clientX - window.innerWidth / 2;
-    yVal = e.clientY - window.innerHeight / 2;
-
-    parallax_el.forEach((el) => {
+    // Function to update the parallax effect based on mouse or device orientation
+    function updateParallax() {
+      parallax_el.forEach((el) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
         el.style.transform = `translateX(calc(-50% + ${xVal * speedx}px)) translateY(calc(-50% + ${yVal * speedy}px))`;
+      });
+    }
+
+    // Add an event listener for the 'mouseover' event
+    window.addEventListener("mousemove", (e) => {
+      xVal = e.clientX - window.innerWidth / 2;
+      yVal = e.clientY - window.innerHeight / 2;
+      updateParallax();
     });
-});
+
+    // Add an event listener for the 'deviceorientation' event
+    window.addEventListener('deviceorientation', (event) => {
+      const beta = event.beta; // Get the device's beta (tilt front-to-back) angle
+      const gamma = event.gamma; // Get the device's gamma (tilt left-to-right) angle
+
+      // Calculate the x and y values based on device orientation
+      xVal = gamma;
+      yVal = beta;
+      updateParallax();
+    });
+
+    // Prevent scrolling on mobile devices when tilting
+    document.body.addEventListener('touchmove', (e) => {
+      e.preventDefault();
+    });
